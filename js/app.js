f660ee034e7cbe05d20c0fc661da484192b7bf73
farhutdinoff <farhutdinoff777@gmail.com>
2024-07-11 16:38:35 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$('#menuToggle').click( function(){
+    $('.header-nav').toggleClass("active");
+    $('.search-box').toggleClass("active");
+    $('body').toggleClass("overflow-hidden");
+  });
+
+  $('.product-box__btns button').click( function(){
+    $(this).toggleClass("active");
+  });
+
+  $('.favorites-box button, .btn-favorites').click( function(){
+    $(this).toggleClass("active");
+  });
+  $('.btn-comparison').click( function(){
+    $("input").click();
+    $(this).toggleClass("active");
+  });
+
+  $('.cart-number .minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.cart-number .plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
